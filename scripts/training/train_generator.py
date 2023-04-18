1c01e7651543acf5902d69b74954a2b66bc0273a
Tushaar Gangavarapu <tushaargvsg45@gmail.com>
2023-04-18 02:20:56 -0400

    Add modules to load the pretrained models

--- scripts/training/train_generator.py
+++ scripts/training/train_generator.py
@@ -38,6 +38,7 @@ def main(config_path, base_path_to_store_results, tokenizer_path, train_data_pat
                                **config['transformer']['args'])
     if torch.cuda.device_count() > 1:
         convo_wizard = nn.DataParallel(convo_wizard)
+        convo_wizard.to(device=device)
 
     optimizer = NoamOptimizer(Adam(convo_wizard.module.get_trainable_params(), **config['optimizer']['adam']['args']),
                               embedding_dim=config['transformer']['args']['embedding_dim'],
