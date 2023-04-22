bf9be9cb11a16dd4c1015ff35390a1f7246c68b0
Tushaar Gangavarapu <tushaargvsg45@gmail.com>
2023-04-22 17:12:33 -0400

    Bug fix: segment ids ignore index 0

--- scripts/data_processing/build_tokenizer.py
+++ scripts/data_processing/build_tokenizer.py
@@ -1,28 +1,30 @@
 import os
 from argparse import ArgumentParser
 
+import datasets
 import yaml
 
 from convo_wizard.data_processors.tokenizers.convo_tokenizer import ConvoTokenizer
 
 
-def main(config_path, path_to_hf_dataset, path_to_store_hf_tokenizer):
+def main(config_path, convokit_flat_corpus_hf_filepath, path_to_store_hf_tokenizer):
     with open(config_path, 'r') as fp:
         config = yaml.safe_load(fp)
 
-    tokenizer = ConvoTokenizer(path_to_hf_dataset, **config['tokenizer']['args'])
+    hf_dataset = datasets.load_dataset('json', data_files={'train': convokit_flat_corpus_hf_filepath})
+    tokenizer = ConvoTokenizer(hf_dataset, **config['tokenizer']['args'])
     tokenizer.save(path_to_store_hf_tokenizer)
 
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='generate training dataset (flat conversational corpus)')
     parser.add_argument('--config_path', type=str, help='path to config file')
-    parser.add_argument('--path_to_hf_dataset', type=str, help='path to the huggingface training dataset',
+    parser.add_argument('--convokit_flat_corpus_hf_filepath', type=str, help='path to the huggingface training dataset',
                         default=os.getcwd())
     parser.add_argument('--path_to_store_hf_tokenizer', type=str, help='path to store the tokenizer',
                         default=os.getcwd())
 
     args = parser.parse_args()
 
-    main(config_path=args.config_path, path_to_hf_dataset=args.path_to_hf_dataset,
+    main(config_path=args.config_path, convokit_flat_corpus_hf_filepath=args.convokit_flat_corpus_hf_filepath,
          path_to_store_hf_tokenizer=args.path_to_store_hf_tokenizer)
